cc4fa72fffa2db52502fd06ac4eacb86b8459d5e
André Santos <andrelimasantos21@gmail.com>
2022-07-27 06:15:16 -0300

    condições 1

---
+++ aula08/ex007.js
@@ -0,0 +1,7 @@
+var vel = 55
+
+console.log(`A velocidade de seu carro é ${vel}Km/h.`)
+if(vel > 60){
+    console.log(`Você ultrapassou a velocidade permitida.MULTADO!`)
+}
+console.log(`Dirija sempre com o cinto de segurança!`)
---
+++ aula08/ex008.js
@@ -0,0 +1,7 @@
+var país = 'França'
+console.log(`Vivendo em ${país}`)
+if(país == 'Brasil'){
+    console.log('Eu sou Brasileiro!')
+}else{
+    console.log('Eu sou estrangeiro!')
+}
