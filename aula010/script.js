f6814045d8be49b4b4d2ebdfb180e0aac1463e63
André Santos <andrelimasantos21@gmail.com>
2022-08-04 22:36:58 -0300

    novo

--- aula010/script.js
+++ aula010/script.js
@@ -3,19 +3,19 @@ function carregar() {
     var foto = window.document.getElementById('imagem')
     var data = new Date()
     //var hora = data.getHours( )
-    var hora = 21
+    var hora = 12
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
 
-        foto.innerHTML = ('<img src = "manha.png">')
+        foto.innerHTML = '<img src = "img/manha.png">'
 
     } else if( hora >= 12 && hora < 18) {
 
-        foto.innerHTML = ('<img src = "tarde.png">')
+        foto.innerHTML = '<img src = "tarde.png">'
 
     } else {
 
-        foto.innerHTML = ('<img src = "noite.png">')
+        foto.innerHTML = '<img src = "noite.png">'
     }
 
 }
