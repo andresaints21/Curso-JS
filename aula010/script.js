b1a3403b85ff64ed95c4cf5e51a5569f0a6e1f0c
André Santos <andrelimasantos21@gmail.com>
2022-08-06 06:37:43 -0300

    novo

--- aula010/script.js
+++ aula010/script.js
@@ -1,21 +1,24 @@
 function carregar() { 
     var msg = window.document.getElementById('msg')
-    var foto = window.document.getElementById('imagem')
+    var img = window.document.getElementById('imagem')
     var data = new Date()
     //var hora = data.getHours( )
-    var hora = 12
+    var hora = 20
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
 
-        foto.innerHTML = '<img src = "img/manha.png">'
+        img.src = 'manha.png'
+        document.body.style.background = '#e2cd9f'
 
-    } else if( hora >= 12 && hora < 18) {
+    } else if( hora >= 12 && hora <= 18) {
 
-        foto.innerHTML = '<img src = "tarde.png">'
+        img.src = 'tarde.png'
+        document.body.style.background = '#b9846f'
 
     } else {
 
-        foto.innerHTML = '<img src = "noite.png">'
+        img.src = 'noite.png'
+        document.body.style.background = '#515154'
     }
 
 }
