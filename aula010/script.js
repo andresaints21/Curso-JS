0910a0022112b3f76ae3a52784dde5dff90ec34e
André Santos <andrelimasantos21@gmail.com>
2022-08-08 06:55:54 -0300

    Resolução

--- aula010/script.js
+++ aula010/script.js
@@ -3,21 +3,21 @@ function carregar() {
     var img = window.document.getElementById('imagem')
     var data = new Date()
     //var hora = data.getHours( )
-    var hora = 20
+    var hora = 8
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
 
-        img.src = 'manha.png'
+        img.src = ('img/manha.png')
         document.body.style.background = '#e2cd9f'
 
     } else if( hora >= 12 && hora <= 18) {
 
-        img.src = 'tarde.png'
+        img.src = ('img/tarde.png')
         document.body.style.background = '#b9846f'
 
     } else {
 
-        img.src = 'noite.png'
+        img.src = ('img/noite.png')
         document.body.style.background = '#515154'
     }
 
